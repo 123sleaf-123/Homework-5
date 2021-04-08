0db9885c5d33fe4f06e6a057daf98c8e03d44566
sleaf <80097831+123sleaf-123@users.noreply.github.com>
2021-04-08 17:02:02 +0800

    ++count
    第一次修改

--- ch5-05.c
+++ ch5-05.c
@@ -5,15 +5,16 @@ int main()
     float total = 0.0;
     float grade = 0.0;
     int count = 0;
-    while (count < 30)
+    while (++count =< 30)
     {
         printf("Enter a grade: ");
         scanf("%f", &grade);
-        if (grade < 0 || grade > 100)
+        if (grade < 0 || grade > 100){
+            count = count - 1;/* avoid count increases without adding grade to total */
             continue; /* skip the statements below*/
+        }
 
         total = total + grade;
-        count = count + 1;
         /* continue jumps to here */
     }
 
